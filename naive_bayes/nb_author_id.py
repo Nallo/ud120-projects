361c4ff7b1ef1edb8143457103c875911e375a1f
Stefano Martinallo <stefano@sherlock.bike>
2016-03-11 22:47:47 +0100

    Compute the accuracy of the NB
    * import the GaussianNB module
    * create the GaussianNB classifier
    * fit the classifier on the trainig set
    * predict some data from the classifier
    * calculate the accuracy of the classifier
    
    Signed-off-by: Stefano Martinallo <stefano@sherlock.bike>

--- naive_bayes/nb_author_id.py
+++ naive_bayes/nb_author_id.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python
 
-""" 
-    This is the code to accompany the Lesson 1 (Naive Bayes) mini-project. 
+"""
+    This is the code to accompany the Lesson 1 (Naive Bayes) mini-project.
 
     Use a Naive Bayes Classifier to identify emails by their authors
-    
+
     authors and labels:
     Sara has label 0
     Chris has label 1
 """
-    
+
 import sys
 from time import time
 sys.path.append("../tools/")
@@ -27,7 +27,23 @@ features_train, features_test, labels_train, labels_test = preprocess()
 #########################################################
 ### your code goes here ###
 
+### import the sklearn module for GaussianNB
+from sklearn.naive_bayes import GaussianNB
+
+### create classifier
+clf = GaussianNB()
+
+### fit the classifier on the training features and labels
+clf.fit(features_train, labels_train)
+
+### use the trained classifier to predict labels for the test features
+pred = clf.predict(features_test)
+
+### calculate and return the accuracy on the test data
+from sklearn.metrics import accuracy_score
+accuracy = accuracy_score(labels_test, pred)
+
+### display the accuracy
+print "Accuracy:", accuracy
 
 #########################################################
-
-
