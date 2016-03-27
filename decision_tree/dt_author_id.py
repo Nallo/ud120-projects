359aeefeac2226b2171649dbd5d3fce1710cd1ff
Stefano Martinallo <stefano@sherlock.bike>
2016-03-28 01:59:24 +0200

    Decision Tree: Add full project
    Signed-off-by: Stefano Martinallo <stefano@sherlock.bike>

--- decision_tree/dt_author_id.py
+++ decision_tree/dt_author_id.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python
 
-""" 
+"""
     This is the code to accompany the Lesson 3 (decision tree) mini-project.
 
-    Use a Decision Tree to identify emails from the Enron corpus by author:    
+    Use a Decision Tree to identify emails from the Enron corpus by author:
     Sara has label 0
     Chris has label 1
 """
-    
+
 import sys
 from time import time
 sys.path.append("../tools/")
@@ -24,8 +24,26 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 #########################################################
 ### your code goes here ###
+from sklearn import tree
+
+### Display the number of features.
+print "Features:", len(features_train[0])
+
+### Build the classifier.
+clf = tree.DecisionTreeClassifier(min_samples_split=40)
+
+### Fit the classifier with the training data.
+clf = clf.fit(features_train, labels_train)
+
+### Make the predictions.
+pred = clf.predict(features_test)
+
+### calculate and return the accuracy on the test data
+from sklearn.metrics import accuracy_score
+accuracy = accuracy_score(labels_test, pred)
+
+### display the accuracy
+print "Accuracy:", accuracy
 
 
 #########################################################
-
-
