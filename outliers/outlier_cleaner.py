6ed230d1d9eb7dec4201ac12d53074126bafb989
Stefano Martinallo <stefano@sherlock.bike>
2016-11-01 00:46:52 +0100

    Removing outliers
    Signed-off-by: Stefano Martinallo <stefano@sherlock.bike>

--- outliers/outlier_cleaner.py
+++ outliers/outlier_cleaner.py
@@ -7,14 +7,17 @@ def outlierCleaner(predictions, ages, net_worths):
         residual errors (difference between the prediction
         and the actual net worth).
 
-        Return a list of tuples named cleaned_data where 
+        Return a list of tuples named cleaned_data where
         each tuple is of the form (age, net_worth, error).
     """
-    
+
     cleaned_data = []
 
-    ### your code goes here
+    ### Loop thorugh the data
+    for idx in xrange(len(predictions)):
+        pred = predictions[idx][0]
+        net_worth = net_worths[idx][0]
+        error = (pred - net_worth) ** 2
+        cleaned_data.append((ages[idx][0], net_worth, error))
 
-    
-    return cleaned_data
-
+    return sorted(cleaned_data, key=lambda x: x[2])[:81]
