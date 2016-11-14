3a7d11398f9703d7e2fc87d1f97246338475d5a3
Stefano Martinallo <stefano@sherlock.bike>
2016-11-14 18:35:17 +0100

    Add TfidfVectorizer
    Signed-off-by: Stefano Martinallo <stefano@sherlock.bike>

--- text_learning/vectorize_text.py
+++ text_learning/vectorize_text.py
@@ -36,30 +36,36 @@ word_data = []
 ### can iterate your modifications quicker
 temp_counter = 0
 
-
 for name, from_person in [("sara", from_sara), ("chris", from_chris)]:
     for path in from_person:
         ### only look at first 200 emails when developing
         ### once everything is working, remove this line to run over full dataset
-        temp_counter += 1
+        # temp_counter += 1
         if temp_counter < 200:
             path = os.path.join('..', path[:-1])
             print path
             email = open(path, "r")
 
             ### use parseOutText to extract the text from the opened email
+            email_text = parseOutText(email)
 
             ### use str.replace() to remove any instances of the words
             ### ["sara", "shackleton", "chris", "germani"]
+            email_text = email_text.replace("sara", "")
+            email_text = email_text.replace("shackleton", "")
+            email_text = email_text.replace("chris", "")
+            email_text = email_text.replace("germani", "")
 
             ### append the text to word_data
+            word_data.append(email_text)
 
             ### append a 0 to from_data if email is from Sara, and 1 if email is from Chris
-
+            from_data.append(0 if name == "sara" else 1)
 
             email.close()
 
 print "emails processed"
+print word_data[152]
 from_sara.close()
 from_chris.close()
 
@@ -71,5 +77,8 @@ pickle.dump( from_data, open("your_email_authors.pkl", "w") )
 
 
 ### in Part 4, do TfIdf vectorization here
-
-
+from sklearn.feature_extraction.text import TfidfVectorizer
+vectorizer = TfidfVectorizer(stop_words="english", lowercase=True)
+vectorizer.fit_transform(word_data)
+print len(vectorizer.get_feature_names())
+print vectorizer.get_feature_names()[34597]
