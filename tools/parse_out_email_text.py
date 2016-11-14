fb71aa1a2b29f5c72fc5c747f92d4f2dc17afa24
Stefano Martinallo <stefano@sherlock.bike>
2016-11-14 17:48:10 +0100

    Add stemmer to parseOutText
    Signed-off-by: Stefano Martinallo <stefano@sherlock.bike>

--- tools/parse_out_email_text.py
+++ tools/parse_out_email_text.py
@@ -8,12 +8,12 @@ def parseOutText(f):
         metadata block at the top
         (in Part 2, you will also add stemming capabilities)
         and return a string that contains all the words
-        in the email (space-separated) 
-        
+        in the email (space-separated)
+
         example use case:
         f = open("email_file_name.txt", "r")
         text = parseOutText(f)
-        
+
         """
 
 
@@ -28,19 +28,28 @@ def parseOutText(f):
         text_string = content[1].translate(string.maketrans("", ""), string.punctuation)
 
         ### project part 2: comment out the line below
-        words = text_string
+        #words = text_string
 
         ### split the text string into individual words, stem each word,
         ### and append the stemmed word to words (make sure there's a single
         ### space between each stemmed word)
-        
 
+        ### 1. Remove leading and trailing whitespaces
+        stripped_string = text_string.strip()
 
+        ### 2. Split the string into single words and remove empty words
+        filtered_words = [w for w in stripped_string.split()]
 
+        ### 3. Stemming using list comprehensions
+        ### http://www.secnetix.de/olli/Python/list_comprehensions.hawk
+        from nltk.stem.snowball import SnowballStemmer
+        stemmer = SnowballStemmer("english")
+        stemmed_words = [stemmer.stem(w) for w in filtered_words]
+        words = ' '.join(stemmed_words)
 
     return words
 
-    
+
 
 def main():
     ff = open("../text_learning/test_email.txt", "r")
@@ -51,4 +60,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
