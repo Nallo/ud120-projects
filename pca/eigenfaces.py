e3229963de619567f91026774d2f7ba36ada2d1e
Stefano Martinallo <stefano@sherlock.bike>
2016-11-19 18:06:11 +0100

    Complete PCS lesson
    Signed-off-by: Stefano Martinallo <stefano@sherlock.bike>

--- pca/eigenfaces.py
+++ pca/eigenfaces.py
@@ -81,6 +81,8 @@ X_train_pca = pca.transform(X_train)
 X_test_pca = pca.transform(X_test)
 print "done in %0.3fs" % (time() - t0)
 
+print "variance of the first PC %f" % pca.explained_variance_ratio_[0]
+print "variance of the second PC %f" % pca.explained_variance_ratio_[1]
 
 ###############################################################################
 # Train a SVM classification model
